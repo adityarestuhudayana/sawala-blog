43bbfb4fd3f4dd8f57bf13652596e1e4b6362469
Aditya Restu <adityarestuhudayana@gmail.com>
2024-09-24 19:37:53 +0700

    feat: register

---
+++ src/validation/auth-validation.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+
+export const registerValidation = [
+    body("name").notEmpty().withMessage("name is required"),
+    body("email")
+        .notEmpty().withMessage("email is required")
+        .bail()
+        .isEmail().withMessage("email format not valid"),
+    body("password").notEmpty().withMessage("password is required"),
+    (req: Request, res: Response, next: NextFunction) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                message: "Validation error",
+                errors: errors.array()
+            })
+        }
+        next()
+    }
+];
