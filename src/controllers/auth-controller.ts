050a82cd3395585a1b9b1e8c87ee5bd57a2cc7d1
Aditya Restu <adityarestuhudayana@gmail.com>
2024-09-24 19:37:53 +0700

    feat: register

---
+++ src/controllers/auth-controller.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response } from "express";
+import { RegisterRequest } from "../types/user";
+import { prisma } from "../config/database";
+import bcrypt from 'bcrypt';
+
+export const register = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const request: RegisterRequest = req.body;
+
+        const isEmailAlreadyUsed = await prisma.user.findFirst({
+            where: {
+                email: request.email,
+            }
+        });
+
+        if (isEmailAlreadyUsed) {
+            return res.status(400).json({
+                message: "User with this email already exists",
+            });
+        }
+
+        request.password = await bcrypt.hash(request.password, 10);
+
+        const user = await prisma.user.create({
+            data: request,
+            select: {
+                id: true,
+                name: true,
+                email: true,
+            }
+        });
+
+        res.status(201).json(user);
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json({ message: error.message });
+        } else {
+            res.status(500).json({ message: "Internal server error" });
+        }
+    }
+}
