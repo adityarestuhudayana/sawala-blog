12211623d2a1850d1a4ac3227a5314770912a4ab
Ilham Rizki Febrian <84845759+ilhamgalileo@users.noreply.github.com>
2024-09-26 10:36:08 +0700

    Dev ilham (#6)
    * fitur logout
    
    * cors added
    
    * logout lint fix

--- src/routes/auth-route.ts
+++ src/routes/auth-route.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { login, register } from "../controllers/auth-controller";
+import { login, logout, register } from "../controllers/auth-controller";
 import { loginValidation, registerValidation } from "../validation/auth-validation";
 
 const router = express.Router();
 
 router.post("/auth/register", registerValidation, register);
 router.post("/auth/login", loginValidation, login);
+router.post("/auth/logout", logout);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express, { Request, Response } from 'express';
 import authRoutes from "./routes/auth-route";
 import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
+import cors from 'cors'
 
 const app = express();
 dotenv.config();
+app.use(cookieParser());
 app.use(express.json());
+app.use(cors({origin: ["http://localhost:5173"], credentials: true}))
 
 app.use("/api", authRoutes);
 
