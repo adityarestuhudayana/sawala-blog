1a37c042de96c2f48f54f35c7657d7f6919395b6
Aditya Restu Hudayana <155047795+adityarestuhudayana@users.noreply.github.com>
2024-09-26 15:37:03 +0700

    feat: update profile

---
+++ prisma/migrations/20240926044338_fix_schema_2/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `users` ADD COLUMN `profilePicturePublicId` TEXT NULL;
--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -1,10 +1,17 @@
+import { Request } from "express";
+
 export type RegisterRequest = {
     name: string;
     email: string;
-    password: string
+    password: string;
+    profilePicture?: string;
 };
 
 export type LoginRequest = {
     email: string;
     password: string;
 };
+
+export interface UserRequest extends Request {
+    user?: { id: number, name: string, email: string, profilePicture?: string }
+}
