d1622c1d24dd29022f947c1f811f10cdf829f93a
Ilham Rizki Febrian <ilhamrizkii215@gmail.com>
2024-09-27 17:47:24 +0700

    delete post revisi

--- src/routes/post-route.ts
+++ src/routes/post-route.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { verifyToken } from "../middlewares/verifyToken";
-import { create, getRecommendation, getNewest } from "../controllers/post-controller";
+import { create, getRecommendation, getNewest, deletePost } from "../controllers/post-controller";
 import { createValidation } from "../validation/post-validation";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post("/posts", verifyToken, createValidation, create);
 router.get("/posts/latest", verifyToken, getNewest)
 router.get("/posts/recommendation", verifyToken, getRecommendation);
+router.delete("/posts/:id", verifyToken, deletePost)
 
 export default router;
